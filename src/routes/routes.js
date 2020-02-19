8a85e93ca635ea477076e73b7932430e6d77aa2c
Arthur <arthur.gallina@gmail.com>
2020-02-18 23:00:57 -0300

    add frontend & login page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/authController')
+const friendController = require('../controllers/friendController')
 const authMiddleware = require('../middlewares/auth')
 const routes = express.Router();
 
 
 routes.post('/user/register', authController.store);
 routes.post('/user/auth', authController.auth);
-routes.use(authMiddleware);
 routes.get('/user/show', authController.index);
+routes.use(authMiddleware);
+
+
+routes.get('/main/friends', friendController.index)
+routes.post('/profile/:id/', friendController.store)
 
 module.exports = routes;
