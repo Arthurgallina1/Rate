41568e43ee2fd1a67ee161d7c46a1277e4a45f1e
Arthur <arthur.gallina@gmail.com>
2020-03-09 22:57:43 -0300

    /

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController')
 const friendController = require('../controllers/friendController')
+const dashboardController = require('../controllers/dashboardController')
 const authMiddleware = require('../middlewares/auth')
 const routes = express.Router();
 
@@ -13,5 +14,7 @@ routes.use(authMiddleware);
 
 routes.get('/main/friends', friendController.index)
 routes.post('/profile/:id/', friendController.store)
+
+routes.get('/dashboard/:id/', dashboardController.index)
 
 module.exports = routes;
