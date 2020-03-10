352da8b8c9a61b5fffe082853f72c630162243ea
Arthur <arthur.gallina@gmail.com>
2020-03-09 22:57:43 -0300

    /

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
 import './Login.css'
+import api from '../utils/api'
 // import { Container } from './styles';
-import { FaUserAlt, FaLock } from 'react-icons' 
+// import { FaUserAlt, FaLock } from 'react-icons' 
 
 
-export default function Login() {
+export default function Login( { history }) {
     const [username, setUsername ] = useState('');
     const [password, setPassword ] = useState('');
+    const [loginFail, setLoginFail ] = useState(false);
 
-    function handleLogin(){
-        console.log(username);
-        console.log(password);
+    async function handleLogin(e){
+        e.preventDefault();
 
+        try {
+            const response = await api.post('/user/auth', { 
+                username,
+                password
+            })
+            const idLoggedUser = response.data.user._id;
+            localStorage.setItem('token', response.data.token);
+            history.push(`/dashboard/${idLoggedUser}`)
+        } catch (err) {
+            setLoginFail(true);
+            
+        }
     }
-    
-    
 
 
-  return (
+    return (
     <div>
         <div className="form-wrapper">
+        <div className="error-alert">
+            { loginFail ? <h1> Error on Login, please try again. </h1> : '' } 
+        
+        </div>
         <div className="sign-up"> <p> Signup Now</p> </div>
             <form onSubmit={handleLogin}>
                 <div className="form-interior">
-                    <input type="text" name="username" placeholder="Username" onChange={(e) => { setUsername(e.target.value) }} value={(e) => e.target.value}></input>
-                    <input type="password" name="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} value={(e) => e.target.value}></input>
+                    <input type="text" name="username" placeholder="Username" onChange={(e) => { setUsername(e.target.value) }} value={username}></input>
+                    <input type="password" name="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} value={password}></input>
                     <button type="submit" >Login</button>
                     <p>Create an account.</p>
                 </div>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Login} />
+            <Route exact path="/dashboard/:id" component={Dashboard} />
+            <Route exact path="/profile" component={Profile} />
         </Switch>
     
     </BrowserRouter>
