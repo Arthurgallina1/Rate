49608a38442253fcc2043701170247ec62ce5588
Arthur <arthur.gallina@gmail.com>
2020-03-11 23:06:31 -0300

    add redux, sagas & reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-
+import './config/ReactotronConfig';
 import Routes from './routes/index';
+import store from './store';
 import history from './utils/history';
-
+import { Provider } from 'react-redux'
 
 import GlobalStyle from './styles/global';
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+             </Router>
+        </Provider>
     )
 }
 
