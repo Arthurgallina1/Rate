1f1888072e43ff546d0733e85ca205ca1ed59ebb
Arthur <arthur.gallina@gmail.com>
2020-03-16 20:17:42 -0300

    add multer, post creation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('../config/multer');
 const authController = require('../controllers/authController')
 const friendController = require('../controllers/friendController')
 const friendshipController = require('../controllers/friendshipController')
-const dashboardController = require('../controllers/dashboardController')
+const dashboardController = require('../controllers/dashboardController');
+const postController = require('../controllers/postController');
 const authMiddleware = require('../middlewares/auth')
 const routes = express.Router();
-
+const upload = multer({dest: '../../tmp/uploads'})
 
 routes.post('/user/register', authController.store);
 routes.post('/user/auth', authController.auth);
@@ -15,7 +18,8 @@ routes.get('/user/info/:id', authController.show);
 routes.post('/add', friendshipController.store);
 routes.post('/user/remove', friendshipController.remove);
 routes.get('/main/friends', friendController.index)
-routes.post('/profile/:id/', friendController.store)
+routes.post('/profile/:id/', friendController.store);
+routes.post('/post/create', upload.single('file'), postController.store);
 
 routes.get('/dashboard/:id/', dashboardController.index)
 
