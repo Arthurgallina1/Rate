7529bccec0987ec89d1d1005d70450e12168230e
Arthur <arthur.gallina@gmail.com>
2020-03-31 20:37:56 -0300

    heroku

--- src/server.js
+++ src/server.js
@@ -9,18 +9,25 @@ const { uDB, pDB } = require('./config/config')
 const mongoose = require('mongoose')
 
 // mongodb+srv://${uDB}:${pDB}@clustermevn-cztsy.mongodb.net/dev?retryWrites=true&w=majority
-mongoose.connect(`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`, 
+// mongoose.connect(`mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false`, 
+mongoose.connect(`mongodb+srv://${uDB}:${pDB}@clustermevn-cztsy.mongodb.net/dev?retryWrites=true&w=majority`, 
 { 
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const corsConfig = {
+    origin: 'https://upbeat-varahamihira-17c567.netlify.com',
 
+}
+
+const port = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(morgan('common'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 app.use(cors());
 app.use(routes);
-app.listen(8000, '0.0.0.0');
+// app.listen(8000, '0.0.0.0');
+app.listen(port);
 
