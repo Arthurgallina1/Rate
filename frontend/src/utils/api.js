941c64bf6b03a33728fc69b65e46a20705a05f6f
Arthur <arthur.gallina@gmail.com>
2020-04-21 03:11:11 -0300

    add ec2 url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'ec2-3-132-213-109.us-east-2.compute.amazonaws.com:8000',
+  baseURL: 'http://ec2-3-132-213-109.us-east-2.compute.amazonaws.com:8000',
+
   // baseURL: 'https://rate-backend.herokuapp.com',
 });
 
