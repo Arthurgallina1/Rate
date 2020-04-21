3f3d5f61e4ad4403b0fca7d37f1b436e4658cd22
Arthur <arthur.gallina@gmail.com>
2020-04-21 12:20:08 -0300

    rate page style

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-3-132-213-109.us-east-2.compute.amazonaws.com:8000',
+  baseURL: 'https://ec2-3-132-213-109.us-east-2.compute.amazonaws.com:8000',
   // baseURL: 'http://localhost:8000',
   // baseURL: 'https://rate-backend.herokuapp.com',
 });
